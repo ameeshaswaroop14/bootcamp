1ded4bd7179d6ca8b932cdec0d5d8b37953dc0ad
ameeshaswaroop <ameeshaswaroop@tothenew.com>
2020-03-26 16:22:27 +0530

    oauth2 assignment

---
+++ ex2/src/test/java/com/java/security/excercise/excercise/ExcerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.security.excercise.excercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
