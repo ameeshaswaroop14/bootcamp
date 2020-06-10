e8138f812811d6c2d3251215194a2e4613167b91
ameeshaswaroop <ameeshaswaroop@tothenew.com>
2020-06-10 12:44:18 +0530

    ann

--- commerceApp/src/main/java/com/commerceApp/commerceApp/CommerceApp.java
+++ commerceApp/src/main/java/com/commerceApp/commerceApp/CommerceApp.java
@@ -12,11 +12,13 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 
 @SpringBootApplication
 @EnableAsync
 @EnableCaching
+@EnableScheduling
 public class CommerceApp {
     public static final Logger logger = LoggerFactory.getLogger(CommerceApp.class);
 
